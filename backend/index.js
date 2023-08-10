d1bc3c649e922b5b0e4d96349d85941d038631b9
vedurmaliya <141067169+vedurmaliya@users.noreply.github.com>
2023-08-10 16:50:01 +0530

    first

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const API_KEY = process.env.YOUTUBE_API_KEY;
+
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+app.get('/searchVolume', async (req, res) => {
+    const { keyword } = req.query;
+
+    try {
+        const response = await axios.get(
+            `https://www.googleapis.com/youtube/v3/search?q=${keyword}&type=video&part=id&key=${API_KEY}`
+        );
+        const totalResults = response.data.pageInfo.totalResults;
+        console.log(totalResults);
+        res.json({ totalResults });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
